d89cd48b73e40e49890bbf53aaafa857b697570a
Altan013 <altanai@code.edu.az>
2022-03-16 14:12:04 +0400

    asp1

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controlers
+{
+    public class HomeController : Controller
+    {
+      public IActionResult Index()
+        {
+            return View();
+        }
+      public IActionResult About()
+        {
+            return View();
+        }
+      public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Altan()
+        {
+            return View();
+        }
+
+    }
+}
